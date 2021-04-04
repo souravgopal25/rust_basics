d42e554211c6173bbe6aef66d9a1b5297c16e009
Sourav <souravgopal25@gmail.com>
2021-04-04 09:46:13 +0530

    String Started

--- src/string.rs
+++ src/string.rs
@@ -4,12 +4,25 @@ pub fn run(){
     println!("Length {}",hello.len());
     //push char
     hello.push('W');
+    hello.push_str("orld");
 
     //push String
   //  hello.push_str("")
 
-    //capacity in bytes
+    //len in bytes
+    println!("Lenght {}",hello.len());
+    //capacity
     println!("CAPACITY :{}",hello.capacity());
-    println!("{}",hello);
+    //replace
+    println!("Replace :{}",hello.replace("World"," Sourav"));
+
+    //loop through String by whiteSpaces
+    for word in hello.split_whitespace(){
+      println!("{} !",word);
+    }
+
+    
+    
+    //println!("{}",hello);
 
 }
